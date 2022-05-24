4e3337a2845d35d968b53c6c746cef63c262c483
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2022-05-23 22:12:25 -0400

    Initial Commit

---
+++ DefibHeart/DefibHeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefibHeartApp.swift
+//  DefibHeart
+//
+//  Created by Ryan Lintott on 2022-05-23.
+//
+
+import SwiftUI
+
+@main
+struct DefibHeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
